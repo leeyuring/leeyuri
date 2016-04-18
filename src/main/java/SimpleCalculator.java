148af3c3458c856f3b14da6d93dd9f9a7d8c287e
lee yuri <tidlsl1725@naver.com>
2016-04-18 12:55:13 +0900

    처음으로 수정하는 commit
    기본적인 성적 계산 기능만 제공

---
+++ src/main/java/SimpleCalculator.java
@@ -0,0 +1,38 @@
+
+
+public class SimpleCalculator {
+	private int result=0;
+	
+	public void add(int x, int y) {
+		result = x+y;
+	}
+	
+	public void sub(int x, int y) {
+		result = x-y;
+	}
+	
+	
+	public void mul(int x, int y) {
+		result = x*y;
+	}
+
+	public void div(int x, int y) {
+
+		if (y!=0) //분모가 0인경우  
+			result = x/y;
+	}
+	
+	public void inc(int x) {
+  
+			result +=x;
+	}
+	public void dinc(int x) {
+		  
+		result -=x;
+}
+	
+	public int getResult() {
+		return result;
+	}
+
+}
---
+++ src/test/java/SimpleCalculatorTest.java
@@ -0,0 +1,57 @@
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleCalculatorTest {
+
+	@Test
+	public void testAdd() {
+		SimpleCalculator calc = new SimpleCalculator();
+		calc.add(10, 20);
+		assertEquals(30, calc.getResult());
+		
+	}
+	
+	@Test
+	public void testSub() {
+		SimpleCalculator calc = new SimpleCalculator();
+		calc.sub(10, 20);
+		assertEquals(-10, calc.getResult());
+		
+	}
+	
+	@Test
+	public void testMulti() {
+		SimpleCalculator calc = new SimpleCalculator();
+		calc.mul(10, 20);
+		assertEquals(200, calc.getResult());
+	}
+
+	@Test
+	public void testDiv() {
+		SimpleCalculator calc = new SimpleCalculator();
+		calc.div(100, 20);
+		assertEquals(5, calc.getResult());
+		
+	}
+	
+	@Test
+	public void testInc() {
+		SimpleCalculator calc = new SimpleCalculator();
+		calc.inc(100);
+		assertEquals(100, calc.getResult());
+		calc.inc(200);
+		assertEquals(300, calc.getResult());
+	}
+	@Test
+	public void testDinc() {
+		SimpleCalculator calc = new SimpleCalculator();
+		calc.dinc(100);
+		assertEquals(-100, calc.getResult());
+		calc.dinc(200);
+		assertEquals(-300, calc.getResult());
+	}
+
+}
